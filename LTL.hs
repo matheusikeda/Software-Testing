793b898c5e84f891b16fc398b0f380dd2af8caa1
emcardoso <eltonm.cardoso@gmail.com>
2018-07-05 18:47:54 -0300

    ltlchecker

--- LTL.hs
+++ LTL.hs
@@ -42,12 +42,41 @@ checkLTL interp (U l r) ss = let
 checkLTL interp (W l r) ss = checkLTL interp ((U l r) :|: (G l)) ss
 checkLTL interp (R l r) ss = checkLTL interp (W r (l :&: r)) ss
 
--- checkLTLZ :: Interp a -> LTL -> (Bool,[Env a],[Env a]) -> (Bool,[Env a],[Env a]) 
--- checkLTLZ interp TT (_,p,f) = (True,p,f)
--- checkLTLZ interp FF (_,p,f) = (False,p,f)
--- checkLTLZ interp (Atom s) (_,p,f) = (lookupVal interp s f,p,f)
--- checkLTLZ interp (l :&: r) (_,p,f) = ((checkLTLZ interp l f) && (checkLTLZ interp r f),p,f)
--- checkLTLZ interp (l :|: r) (_,p,f) = ((checkLTLZ interp l f) || (checkLTLZ interp r f),p,f)
--- checkLTLZ interp (Not l) (_,p,f) = (not (checkLTLZ interp l f),p,f)
--- checkLTLZ interp (X l) (_,p,x:xs) = checkLTLZ interp l (True,x:p,xs)
+mkMaybe :: Bool -> a -> Maybe a
+mkMaybe True x = Just x
+mkMaybe False _ = Nothing 
+
+(|&>) :: (Bool, Maybe (Env a)) ->  (Bool, Maybe (Env a)) -> (Bool, Maybe (Env a))
+(|&>) (True,_)   (True, _) = (True, Nothing)
+(|&>) (False,r)  _         = (False,r)
+(|&>) _          (False,r) = (False,r)
+
+(|+>) :: (Bool, Maybe (Env a)) ->  (Bool, Maybe (Env a)) -> (Bool, Maybe (Env a))
+(|+>) (False,r)   (False, _) = (False, r)
+(|+>) _           _          = (True,Nothing)
+
+(|!>) :: (Bool, Maybe (Env a)) ->  (Bool, Maybe (Env a))
+(|!>) (True,_) = (True,Nothing)
+(|!>) (False,r) = (False,r)
+
+lstCheck :: [(Bool,Maybe (Env a))] -> (Bool,Maybe (Env a))
+lstCheck xs = case dropWhile fst xs of
+                  [] -> (True,Nothing)
+                  (x:_) -> x
+
+a :: [(Bool,Maybe (Env a))] -> (Bool,Maybe (Env a))
+a xs = 
+
+checkLTLZ :: Interp a -> LTL -> [Env a] -> (Bool,Maybe (Env a)) 
+checkLTLZ interp TT es             = (True,Nothing)
+checkLTLZ interp FF es             = (False,Just $ head es)
+checkLTLZ interp (Atom s)  es      = let r = lookupVal interp s es in (r,mkMaybe r  (head es))
+checkLTLZ interp (l :&: r) es      = (checkLTLZ interp l es) |&> (checkLTLZ interp r es)
+checkLTLZ interp (l :|: r) es      = (checkLTLZ interp l f)  |+> (checkLTLZ interp r f)
+checkLTLZ interp (Not l)   es      = |!> (checkLTLZ interp l es)
+checkLTLZ interp (X l)    (_:xs)   = checkLTLZ interp l xs
+checkLTLZ interp (G l) es          = lstCheck $ map (checkLTLZ interp l .(:[])) es 
+checkLTL interp (U l r) ss = let
+                               ss' = dropWhile (checkLTL interp l . (: [])) ss
+                             in checkLTL interp r ss'
 
